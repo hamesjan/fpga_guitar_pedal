5aa709ac2ca39d943d6fe939ac0d36f82f5e656b
Bizzigotti <mbizzigotti@ieng6-202.ucsd.edu>
2025-10-13 08:19:37 -0700

    remove junk and add test dat files

--- fir/Q4/fir.comp/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,7 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "fir","ID" : "0","Type" : "sequential",
-"SubInsts" : [
-	{"Name" : "grp_fir_Pipeline_Shift_Accum_Loop_fu_53","ID" : "1","Type" : "sequential",
-		"SubLoops" : [
-		{"Name" : "Shift_Accum_Loop","ID" : "2","Type" : "pipeline"},]},]
-}]}
