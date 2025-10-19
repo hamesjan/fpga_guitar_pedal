6f3a825ee0e99f587e6eade914ce3d695ac0d290
Han <jjhan@ieng6-203.ucsd.edu>
2025-10-19 12:34:25 -0700

    making progress 1a

--- cordic/cordic_1a/cordiccart2pol.cpp
+++ cordic/cordic_1a/cordiccart2pol.cpp
@@ -4,7 +4,7 @@ data_t Kvalues[NO_ITER] = {1,	0.500000000000000,	0.250000000000000,	0.1250000000
 
 data_t angles[NO_ITER] = {0.785398163397448,	0.463647609000806,	0.244978663126864,	0.124354994546761,	0.0624188099959574,	0.0312398334302683,	0.0156237286204768,	0.00781234106010111,	0.00390623013196697,	0.00195312251647882,	0.000976562189559320,	0.000488281211194898,	0.000244140620149362,	0.000122070311893670,	6.10351561742088e-05,	3.05175781155261e-05};
 
-void cordic(data_t x, data_t y, theta_t theta, theta_t &accum_theta,  cos_sin_t &s, cos_sin_t &c){
+void cordic(data_t x, data_t y, theta_t &theta, cos_sin_t &c){
 
 	cos_sin_t current_cos = x;
 	cos_sin_t current_sin = y;
@@ -12,24 +12,20 @@ void cordic(data_t x, data_t y, theta_t theta, theta_t &accum_theta,  cos_sin_t 
 	cos_sin_t factor = 1.0;
 
 	for (int j = 0; j < NO_ITER; j++){
-		int sigma = (theta < 0) ? -1 : 1;
+		int sigma = (current_sin < 0) ? -1 : 1;
 
-		cos_sin_t cos_shift = current_cos * sigma * factor;
-		cos_sin_t sin_shift = current_sin * sigma * factor;
+		cos_sin_t cos_shift = current_cos * Kvalues[j];
+		cos_sin_t sin_shift = current_sin * Kvalues[j];
 
 		// rotation
-		current_cos = current_cos - sin_shift;
-		current_sin = current_sin + cos_shift;
+		current_cos = current_cos + sigma * sin_shift;
+		current_sin = current_sin - sigma * cos_shift;
 
-		theta = theta - sigma * angles[j];
-		accum_theta = accum_theta + sigma * angles[j];			
-		factor = factor / 2;
+		theta = theta + sigma * angles[j];				
 		
 	}
 
-	
-	s = current_sin;
-	c = current_cos;
+	c = current_cos * 0.607252935;
 }
 
 void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
@@ -46,12 +42,11 @@ void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 	
 	data_t new_y;
 	data_t new_x;
-
+	theta_t final_angle = 0.0;
 	if (y > 0){
 		// y in quadrant I or II, rotate by negative 90
 		new_x = y;
-		new_y = x * -1;
-				
+		new_y = x * -1;				
 	} else {
 		// y in quadrant III or IV, rotate by 90
 		new_x = y * -1;
@@ -60,9 +55,9 @@ void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 	cos_sin_t final_y;
 	cos_sin_t final_x;
 	theta_t target_angle = 0.0;
-	theta_t final_angle;
 
-	cordic(new_x, new_y, target_angle, final_angle, final_x, final_y);
+
+	cordic(new_x, new_y, final_angle, final_x);
 	*r = final_x;
 	*theta = final_angle;
 	
--- cordic/cordic_1a/cordiccart2pol.h
+++ cordic/cordic_1a/cordiccart2pol.h
@@ -10,7 +10,7 @@ typedef float acc_t;
 typedef float theta_t;
 typedef float cos_sin_t;
 
-void cordic(data_t x, data_t y, theta_t theta, theta_t &accum_theta, cos_sin_t &s, cos_sin_t &c);
+void cordic(data_t x, data_t y, theta_t &theta, cos_sin_t &c);
 void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta);
 
 #endif
