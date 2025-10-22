74b791a3c0bcabe2a8d3fc58e379509df1ac5297
Bizzigotti <mbizzigotti@ieng6-203.ucsd.edu>
2025-10-21 17:40:24 -0700

    fix cordic theta values

--- cordic/cordic_1a/cordiccart2pol.cpp
+++ cordic/cordic_1a/cordiccart2pol.cpp
@@ -1,9 +1,14 @@
+#include <array>
 #include "cordiccart2pol.h"
 
-data_t Kvalues[NO_ITER] = {1,	0.500000000000000,	0.250000000000000,	0.125000000000000,	0.0625000000000000,	0.0312500000000000,	0.0156250000000000,	0.00781250000000000,	0.00390625000000000,	0.00195312500000000,	0.000976562500000000,	0.000488281250000000,	0.000244140625000000,	0.000122070312500000,	6.10351562500000e-05,	3.05175781250000e-05};
+data_t Kvalues[] = {1,   0.500000000000000,      0.250000000000000,      0.125000000000000,      0.0625000000000000,     0.0312500000000000,     0.0156250000000000,     0.00781250000000000,    0.00390625000000000,    0.00195312500000000,    0.000976562500000000,   0.000488281250000000,   0.000244140625000000,   0.000122070312500000,   6.10351562500000e-05,   3.05175781250000e-05, 1.525878e-05, 7.62939e-06 };
 
-data_t angles[NO_ITER] = {0.785398163397448,	0.463647609000806,	0.244978663126864,	0.124354994546761,	0.0624188099959574,	0.0312398334302683,	0.0156237286204768,	0.00781234106010111,	0.00390623013196697,	0.00195312251647882,	0.000976562189559320,	0.000488281211194898,	0.000244140620149362,	0.000122070311893670,	6.10351561742088e-05,	3.05175781155261e-05};
+data_t angles[] = {0.785398163397448,    0.463647609000806,      0.244978663126864,      0.124354994546761,      0.0624188099959574,     0.0312398334302683,     0.0156237286204768,     0.00781234106010111,    0.00390623013196697,    0.00195312251647882,    0.000976562189559320,   0.000488281211194898,   0.000244140620149362,   0.000122070311893670,   6.10351561742088e-05,   3.05175781155261e-05, 1.52587891e-05, 7.62939453e-06 };
 
+static_assert(std::size(Kvalues) >= NO_ITER);
+static_assert(std::size(angles) >= NO_ITER);
+
+// Reference
 void cordic(data_t x, data_t y, theta_t &theta, cos_sin_t &c){
 
 	cos_sin_t current_cos = x;
@@ -40,26 +45,39 @@ void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 	// set by *r = discovered r;	
 	// set by *theta = discovered theta
 	
-	data_t new_y;
-	data_t new_x;
-	theta_t final_angle = 0.0;
-	if (y > 0){
-		// y in quadrant I or II, rotate by negative 90
-		new_x = y;
-		new_y = x * -1;				
-	} else {
-		// y in quadrant III or IV, rotate by 90
-		new_x = y * -1;
-		new_y = x;
+	data_t new_x = x;
+	data_t new_y = y;
+	theta_t new_theta = 0.0;
+	if (x < 0){
+		new_x = x * -1;
+		new_y = y * -1;				
+		if (y < 0)
+			new_theta = -3.141592;
+		else
+			new_theta = 3.141592;
 	}
 	cos_sin_t final_y;
 	cos_sin_t final_x;
-	theta_t target_angle = 0.0;
 
+	cos_sin_t current_cos = new_x;
+	cos_sin_t current_sin = new_y;
+	cos_sin_t factor = 1.0;
 
-	cordic(new_x, new_y, final_angle, final_x);
-	*r = final_x;
-	*theta = final_angle;
+	for (int j = 0; j < NO_ITER; j++){
+		int sigma = (current_sin < 0) ? -1 : 1;
+
+		cos_sin_t cos_shift = current_cos * Kvalues[j];
+		cos_sin_t sin_shift = current_sin * Kvalues[j];
+
+		// rotation
+		current_cos = current_cos + sigma * sin_shift;
+		current_sin = current_sin - sigma * cos_shift;
+
+		new_theta = new_theta + sigma * angles[j];				
+	}
+
+	*r = current_cos * 0.607252935;
+	*theta = new_theta;
 	
 }
 
