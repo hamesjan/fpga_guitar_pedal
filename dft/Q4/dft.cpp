b92461eade2a2f0c05ed5f84629b5e6de33409bc
Scott <bmscott@ieng6-202.ucsd.edu>
2025-11-04 21:36:56 -0800

    initial start of q4 w some outlined opt, not sure if this is right

--- dft/Q4/dft.cpp
+++ dft/Q4/dft.cpp
@@ -1,8 +1,42 @@
 #include<math.h>
 #include "dft.h"
-#include"coefficients32.h"
+#include"coefficients32_2D.h"
 
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE])
+// real and imaginary parts doubled as both inputs and outputs
+// 
+void dft(const DTYPE input_real[SIZE], const DTYPE input_img[SIZE],
+        DTYPE output_real[SIZE], DTYPE output_img[SIZE])
 {
-//Write your code here
+    // 1D arrays: partition along their only dimension (dim=1)
+    #pragma HLS ARRAY_PARTITION variable=input_real  block factor=4 dim=1
+    #pragma HLS ARRAY_PARTITION variable=input_img  block factor=4 dim=1
+    #pragma HLS ARRAY_PARTITION variable=output_real block factor=4 dim=1
+    #pragma HLS ARRAY_PARTITION variable=output_img block factor=4 dim=1
+
+    // 2D tables [k][n] we iterate n in the inner loop (partition dim=2)
+    #pragma HLS ARRAY_PARTITION variable=cos_coeff_table block factor=4 dim=2
+    #pragma HLS ARRAY_PARTITION variable=sin_coeff_table block factor=4 dim=2
+
+    data_loop:
+    for (int k = 0; k < SIZE; k++){
+        DTYPE sum_real = 0;
+        DTYPE sum_imag = 0;
+
+        dot_product_loop:
+        for (int n = 0; n < SIZE; n++){
+            // use LUTs for cosine and sine values
+            DTYPE cos_val = cos_coeff_table[k][n];
+            DTYPE sin_val = sin_coeff_table[k][n];
+
+            // fetch real and imaginary parts
+            DTYPE real_part = input_real[n];
+            DTYPE imag_part = input_img[n];
+
+            #pragma HLS PIPELINE II=1
+            sum_real += real_part * cos_val + imag_part * sin_val;
+            sum_imag += -1 * real_part * sin_val + imag_part * cos_val;
+        }
+        output_real[k] = sum_real;
+        output_img[k] = sum_imag;
+    }
 }
