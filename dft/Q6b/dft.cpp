a15667b5f2a3f8592e7e67eeeec77859020b3644
hamesjan <jhan31903@gmail.com>
2025-11-05 14:06:53 -0800

    done with 6b

--- dft/Q6b/dft.cpp
+++ dft/Q6b/dft.cpp
@@ -4,20 +4,24 @@
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])	//Use pointers while doing the demo for streaming//
 {
-	//Write your code here
-    const double PI = 3.141592653;
-    
     data_loop:
     for (int k = 0; k < SIZE; k++){
         DTYPE sum_real = 0;
         DTYPE sum_imag = 0;
+
         dot_product_loop:
         for (int n = 0; n < SIZE; n++){
-            sum_real += real_sample[n] * cos((2*PI*k*n) / SIZE) + imag_sample[n] * sin((2*PI*k*n) / SIZE );
-            sum_imag += -1 * real_sample[n] * sin((2*PI*k*n) / SIZE) + imag_sample[n] * cos((2*PI*k*n) / SIZE );
+            // use LUTs for cosine and sine values
+	    int idx = (k * n) % SIZE;
+            DTYPE cos_val = cos_coefficients_table[idx];
+            DTYPE sin_val = sin_coefficients_table[idx];
+
+            // fetch real and imaginary parts
+            sum_real += real_sample[n] * cos_val + imag_sample[n] * sin_val;
+            sum_imag += real_sample[n] * sin_val + imag_sample[n] * cos_val;
         }
         real_op[k] = sum_real;
         imag_op[k] = sum_imag;
     }
-}
 
+}
