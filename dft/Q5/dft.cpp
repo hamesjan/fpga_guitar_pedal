1a6e9217c58b8b35a4aecf2e402ac8fe20cb1bfb
Lal <ablal@ieng6-201.ucsd.edu>
2025-11-05 15:19:38 -0800

    Q5 baseline

--- dft/Q5/dft.cpp
+++ dft/Q5/dft.cpp
@@ -7,13 +7,25 @@
 void dft(const DTYPE input_real[SIZE], const DTYPE input_img[SIZE],
         DTYPE output_real[SIZE], DTYPE output_img[SIZE])
 {
+    // 1D arrays: partition along their only dimension (dim=1)
+    #pragma HLS ARRAY_PARTITION variable=input_real  block factor=32 dim=1
+    #pragma HLS ARRAY_PARTITION variable=input_img  block factor=32 dim=1
+    #pragma HLS ARRAY_PARTITION variable=output_real block factor=32 dim=1
+    #pragma HLS ARRAY_PARTITION variable=output_img block factor=32 dim=1
+
+    // 2D tables [k][n] we iterate n in the inner loop (partition dim=2)
+    #pragma HLS ARRAY_PARTITION variable=cos_coeff_table block factor=32 dim=2
+    #pragma HLS ARRAY_PARTITION variable=sin_coeff_table block factor=32 dim=2
+
     data_loop:
     for (int k = 0; k < SIZE; k++){
+        #pragma HLS PIPELINE II=1
         DTYPE sum_real = 0;
         DTYPE sum_imag = 0;
 
         dot_product_loop:
         for (int n = 0; n < SIZE; n++){
+            #pragma HLS PIPELINE II=1
             // use LUTs for cosine and sine values
             DTYPE cos_val = cos_coeff_table[k][n];
             DTYPE sin_val = sin_coeff_table[k][n];
