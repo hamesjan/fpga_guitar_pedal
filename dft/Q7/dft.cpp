11032823c160d342ea5c345e7e710c5f444a8536
hamesjan <jhan31903@gmail.com>
2025-11-05 14:45:54 -0800

    Q7

--- dft/Q7/dft.cpp
+++ dft/Q7/dft.cpp
@@ -4,5 +4,19 @@
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])	//Use pointers while doing the demo for streaming//
 {
-	//Write your code here
+    data_loop:
+    for (int n = 0; n < SIZE; n++){
+	dot_product_loop:
+        for (int k = 0; k < SIZE; k++){
+            // use LUTs for cosine and sine values
+	    int idx = (k * n) % SIZE;
+            DTYPE cos_val = cos_coefficients_table[idx];
+            DTYPE sin_val = sin_coefficients_table[idx];
+
+            // fetch real and imaginary parts
+            real_op[k] += real_sample[n] * cos_val + imag_sample[n] * sin_val;
+            imag_op[k] += real_sample[n] * sin_val + imag_sample[n] * cos_val;
+        }
+    }
+
 }
